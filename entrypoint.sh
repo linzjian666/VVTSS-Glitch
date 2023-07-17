b4f861b601ee6e568397c9b69148a3b135566c6c
linzjian666 <63142068+linzjian666@users.noreply.github.com>
2023-07-17 19:03:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 WSPATH=${WSPATH:-'glitch'}  # WS 路径前缀。(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
-WEB_PASSWORD=${WEB_PASSWORD:-'password'}
+WEB_PASSWORD=${WEB_PASSWORD:-'passwd@123'}
 
 # 生成 web.js 配置文件
 generate_config() {
